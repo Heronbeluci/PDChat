898c1616af61610d7c90dbe94526919052993e0e
Beluci <heron.srs@gmail.com>
2021-03-31 10:34:39 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang='pug'>
+  #app
+    Main(msg="Hello World")
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style lang="scss">
+$teste: red;
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+  color: $teste;
+}
+</style>
